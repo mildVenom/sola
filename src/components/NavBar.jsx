c101616301f8ad1a63546c2b7dd50db2c83adc13
Antony Gakuru <muriithigakuru@gmail.com>
2024-01-18 14:31:59 +0300

    add components, logo & navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <nav className='container'>
+        <img src="" alt="school of life art logo" className='' />
+    </nav>
+  )
+}
+
+export default NavBar
