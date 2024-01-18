3a47b21771f7699241a3a799f19de2fe885508b9
Antony Gakuru <muriithigakuru@gmail.com>
2024-01-18 15:06:36 +0300

    add landing page & note to self

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import icon from '../assets/images/icon.png';
 
 function NavBar() {
   return (
     <nav className='container'>
-        <img src="" alt="school of life art logo" className='' />
+        <img src={icon} alt="school of life art logo" className='h-20 w-20' />
+        
     </nav>
   )
 }
---
+++ src/components/landingPage/hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function hero() {
+  return (
+    <section className=''>hero</section>
+  )
+}
+
+export default hero
