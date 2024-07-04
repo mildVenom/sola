6739bab7a6e3d202767b3d1ab9b2737bf7387a76
Antony Gakuru <antony123muriithi@gmail.com>
2024-07-04 14:35:21 +0300

    fix navbar stoopead bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,47 +11,51 @@ const Navigation = () => {
 
     return (
         <nav className='bg-white flex items-center justify-between gap-3 px-4 py-2 md:py-2 md:px-36 shadow top-0  w-full z-50'>
-            <img src={icon} alt="school of life art logo" className='h-30 w-60' />
+            <Link to='/'>
+                <img src={icon} alt="school of life art logo" className='h-[90px] w-50 py-2' />
+            </Link>
 
-            <div className={`nav-list middleNav ${isNavOpen ? 'block' : 'hidden'} md:flex`}>
+            <div className={`nav-list middleNav hidden md:block`}>
                 <ul className="flex flex-col md:flex-row gap-4 md:gap-16 items-center justify-center md:py-0 py-8 font-medium md:text-md text-gray-700">
                     <li className="nav-li active no-underline capitalize hover:text-orange-700">
-                        <Link to="/"  className=" nav-li active:text-orange-500">Home</Link>
-                    </li> 
-                    <li className=" no-underline capitalize hover:text-orange-700">
-                        <Link to="/about"  className=" nav-li active:text-orange-500">About</Link>
-                    </li> 
-                    <li className=" no-underline capitalize hover:text-orange-700 hover:rounded-full">
-                        <Link to="/whatWeOffer" className=" nav-li active:text-orange-500 text-nowrap">What we offer</Link>
+                        <Link to="/" className="nav-li active:text-orange-500">Home</Link>
+                    </li>
+                    <li className="no-underline capitalize hover:text-orange-700">
+                        <Link to="/about" className="nav-li active:text-orange-500">About</Link>
+                    </li>
+                    <li className="no-underline capitalize hover:text-orange-700 hover:rounded-full">
+                        <Link to="/whatWeOffer" className="nav-li active:text-orange-500 text-nowrap">What we offer</Link>
                     </li>
                 </ul>
             </div>
 
-            <div className="md:hidden">
-                <button
-                    onClick={toggleNav}
-                    className="text-white focus:outline-none"
-                >
-                    {isNavOpen ? <box-icon name='x'></box-icon> : <box-icon name='menu-alt-right'></box-icon>}
-                </button>
-            </div>
+            <div className="flex justify-center items-center flex-row-reverse">
+                <div className="md:hidden">
+                    <button
+                        onClick={toggleNav}
+                        className="text-white focus:outline-none"
+                    >
+                        {isNavOpen ? <box-icon name='x'></box-icon> : <box-icon name='menu-alt-right'></box-icon>}
+                    </button>
+                </div>
 
-            <div className={`md:hidden ${isNavOpen ? 'block' : 'hidden'}`}>
-                <ul className="flex flex-col justify-center items-center mt-4">
-                    <li>
-                        <Link to="/" className="active block py-2 px-4 text-orange-500 hover:bg-orange-500 hover:text-white">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/about' className='block py-2 px-4 text-orange-500 hover:bg-orange-500 hover:text-white'>About</Link>
-                    </li>
-                    <li>
-                        <Link to="/whatWeOffer" className="block py-2 px-4 text-orange-500 hover:bg-orange-500 hover:text-white">
-                            What we offer
-                        </Link>
-                    </li>
-                </ul>
+                <div className={`md:hidden ${isNavOpen ? 'block' : 'hidden'}`}>
+                    <ul className="flex flex-col justify-center items-center mt-4">
+                        <li>
+                            <Link to="/" className="active block py-2 px-4 text-orange-500 hover:bg-orange-500 hover:text-white">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/about' className='block py-2 px-4 text-orange-500 hover:bg-orange-500 hover:text-white'>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/whatWeOffer" className="block py-2 px-4 text-orange-500 hover:bg-orange-500 hover:text-white">
+                                What we offer
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
             <button className="hidden md:block bg-orange-500 hover:bg-orange-700 text-nowrap sm:text-md text-white py-2 px-4 rounded-full capitalize font-semibold transition duration-300 ease-in-out">
